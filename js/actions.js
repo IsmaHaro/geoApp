244d9d7a321eaa0b1a219ae6f141fa0e30333fd8
Isma <ismaharo>
2015-07-25 14:45:07 -0500

    Nuevo proyecto geoApp

---
+++ js/actions.js
@@ -0,0 +1,23 @@
+var fn = {
+	init: function initialize() {
+		//Posición del mapa
+		geo.getPosition();
+		var latlng = new google.maps.LatLng(geo.lat, geo.lng);
+		var myOptions = {
+			zoom: 18,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+		//Marcador
+		var marker = new google.maps.Marker({
+			position: latlng, 
+			map: map, 
+			title:"Mi posición"
+		});
+	},
+	ready: function(){
+		document.addEventListener("deviceready", fn.init, false);
+	}
+};
+$(fn.ready);
