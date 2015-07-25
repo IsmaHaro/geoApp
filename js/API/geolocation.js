9e5237c3f0a527119b31c96c8fedc785123cb735
Isma <ismaharo>
2015-07-25 15:15:35 -0500

    add

--- js/API/geolocation.js
+++ js/API/geolocation.js
@@ -1,11 +1,6 @@
 var geo = {
 	lat: null,
 	lng: null,
-	getPosition: function(){
-		alert("Obteniendo posicion");
-		navigator.geolocation.getCurrentPosition(geo.success, geo.error);
-		alert("Latitud: "+geo.lat+" y Longitud: "+geo.lng);
-	},
 	error: function(err){
 		alert(err.code);
 	},
@@ -13,6 +8,10 @@ var geo = {
 		alert("Latitud: "+p.coords.latitude);
 		geo.lat = p.coords.latitude;
 		geo.lng = p.coords.longitude;
+	},
+	getPosition: function(){
+		alert("Obteniendo posicion");
+		navigator.geolocation.getCurrentPosition(geo.success, geo.error);
+		alert("Latitud: "+geo.lat+" y Longitud: "+geo.lng);
 	}
-
 };
